056ac11799cf083bfdeb70bee9ae06e521eae5df
alex <alejandrosilvarodriguez9@gmail.com>
2024-02-28 22:15:50 +0100

    basal metabolic rate implementado

--- src/main/java/healthcalc/HealthCalcImpl.java
+++ src/main/java/healthcalc/HealthCalcImpl.java
@@ -8,34 +8,44 @@ public class HealthCalcImpl implements HealthCalc {
             throw new RuntimeException("altura negativa");
         }
         gender=Character.toUpperCase(gender);
-        float IW;
+        double IW;
         if(gender!='M' || gender!='F'){
             throw new RuntimeException("Genero incorrecto");
         }else if(gender=='M'){
             IW = height - 100 - (height - 150) / 4;
         }else{
-            IW = (float) (height - 100 - (height - 150) / 2.5);
+            IW = (height - 100 - (height - 150) / 2.5);
         }
         
-	    return IW;
+	    return (float) IW;
 	 
 	 
     }
     public float basalMetabolicRate(float weight, int height, char gender, int age) throws Exception{
-       
-        return 0;
-        /**
-	 * Calculate the Basal Metabolic Rate (BMR) of a person with the following formula:
-	 * For men: BMR = 10 * weight + 6.25 * height - 5 * age + 5
-	 * For women: BMR = 10 * weight + 6.25 * height - 5 * age - 161
-	 *   
-	 * @param weight	Weight of the person (kg).
-	 * @param height 	Height of the person (cm).
-	 * @param gender	Gender of the person ('m' or 'w').
-	 * @param age		Age of the person.
-	 * @return	  		The Basal Metabolic Rate of the person.
-	 * @throws Exception
-	 */
+        // Calculate the Basal Metabolic Rate (BMR) of a person with the following formula:
+
+        if(Float.toString(weight).valueOf(0).equals("-")){
+            throw new RuntimeException("peso negativo");
+        }
+        if(Integer.signum(height)=='-'){
+            throw new RuntimeException("altura negativa");
+        }
+        if(Integer.signum(age)=='-'){
+            throw new RuntimeException("edad negativa");
+        }
+        if(gender!='M' || gender!='F'){
+            throw new RuntimeException("Genero incorrecto");
+        }
+        double BMR;
+        gender=Character.toUpperCase(gender);
+        if(gender=='M'){
+            BMR = 10 * weight + 6.25 * height - 5 * age + 5;
+        }else{
+            BMR = 10 * weight + 6.25 * height - 5 * age - 161;
+        }
+        return (float)BMR;
+    
+	 
     }
 	
 }
