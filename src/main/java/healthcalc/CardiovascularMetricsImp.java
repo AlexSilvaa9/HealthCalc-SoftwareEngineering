8e602dfeafbdf0025a27e5a027e8d1660d7cf4bd
alex <alejandrosilvarodriguez9@gmail.com>
2024-05-13 18:05:08 +0200

    clase personImp cardiovascular metric cambiada

--- src/main/java/healthcalc/CardiovascularMetricsImp.java
+++ src/main/java/healthcalc/CardiovascularMetricsImp.java
@@ -2,18 +2,18 @@ package healthcalc;
 
 public class CardiovascularMetricsImp implements CardiovascularMetrics{
 
-    public double getIdealWeight(int height, Gender gender) throws Exception{
+    public double getIdealWeight(Person person) throws Exception{
         //  * Calculate the ideal weight (IW) of a person following the Lorentz formula:
-            if(height<=0){
+            if(person.height()<=0){
                 throw new RuntimeException("altura menor o igual que cero");
             }
             double IW;
-            if(!(gender.equals(Gender.MALE) || gender.equals(Gender.FEMALE))){
+            if(!(person.gender().equals(Gender.MALE) || person.gender().equals(Gender.FEMALE))){
                 throw new RuntimeException("Genero incorrecto");
-            }else if(gender.equals(Gender.MALE)){
-                IW = height - 100 - (height - 150) / 4;
+            }else if(person.gender().equals(Gender.MALE)){
+                IW = person.height() - 100 - (person.height() - 150) / 4;
             }else{
-                IW = (height - 100 - (height - 150) / 2.5);
+                IW = (person.height() - 100 - (person.height() - 150) / 2.5);
             }
             
             return  IW;
