666f1b5477b2a2836060d437499accf13ef54e79
alex <alejandrosilvarodriguez9@gmail.com>
2024-05-13 17:14:07 +0200

    interfaces separadas y probadas

--- src/main/java/healthcalc/HealthCalcImpl.java
+++ src/main/java/healthcalc/HealthCalcImpl.java
@@ -18,51 +18,12 @@ public class HealthCalcImpl implements HealthCalc {
     }
     public float idealWeight(int height, char gender) throws Exception{
     //  * Calculate the ideal weight (IW) of a person following the Lorentz formula:
-        if(height<=0){
-            throw new RuntimeException("altura menor o igual que cero");
-        }
-        gender=Character.toUpperCase(gender);
-        double IW;
-        if(!(gender=='M' || gender=='F')){
-            throw new RuntimeException("Genero incorrecto");
-        }else if(gender=='M'){
-            IW = height - 100 - (height - 150) / 4;
-        }else{
-            IW = (height - 100 - (height - 150) / 2.5);
-        }
-        
-	    return (float) IW;
-	 
+       return cardiovascularMetrics.getIdealWeight(height,gender);
 	 
     }
     public float basalMetabolicRate(float weight, int height, char gender, int age) throws Exception{
         // Calculate the Basal Metabolic Rate (BMR) of a person with the following formula:
-        gender=Character.toUpperCase(gender);
-
-        if(weight <= 0){
-            throw new RuntimeException("peso menor o igual que cero");
-        }
-        if(height <=0){
-            throw new RuntimeException("altura menor o igual que cero");
-        }
-        if(age <= 0){
-            throw new RuntimeException("edad menor o igual que cero");
-        }
-        if(!(gender=='M' || gender=='F')){
-            throw new RuntimeException("Genero incorrecto");
-        }
-        double BMR;
-        if(gender=='M'){
-            BMR = 10 * weight + 6.25 * height - 5 * age + 5;
-        }else{
-            BMR = 10 * weight + 6.25 * height - 5 * age - 161;
-        }
-        // cuando se desborda pasa a ser infinito
-        float fBMR=(float)BMR;
-        if(Float.isInfinite(fBMR)){
-            throw new ArithmeticException("Se ha desbordado");
-        }
-        return fBMR;
+        return metabolicMetrics.basalMetabolicRate(weight, height, gender, age);
     
 	 
     }
