0b852c2f970e45798181905715978f6e22afeb41
alex <alejandrosilvarodriguez9@gmail.com>
2024-05-13 17:09:23 +0200

    MetabolicMetricsImp impplementado

--- src/main/java/healthcalc/MetabolicMetricsImp.java
+++ src/main/java/healthcalc/MetabolicMetricsImp.java
@@ -1,5 +1,36 @@
 package healthcalc;
 
-public class MetabolicMetricsImp {
+public class MetabolicMetricsImp implements MetabolicMetrics{
 
+    public float basalMetabolicRate(float weight, int height, char gender, int age) throws Exception{
+        // Calculate the Basal Metabolic Rate (BMR) of a person with the following formula:
+        gender=Character.toUpperCase(gender);
+
+        if(weight <= 0){
+            throw new RuntimeException("peso menor o igual que cero");
+        }
+        if(height <=0){
+            throw new RuntimeException("altura menor o igual que cero");
+        }
+        if(age <= 0){
+            throw new RuntimeException("edad menor o igual que cero");
+        }
+        if(!(gender=='M' || gender=='F')){
+            throw new RuntimeException("Genero incorrecto");
+        }
+        double BMR;
+        if(gender=='M'){
+            BMR = 10 * weight + 6.25 * height - 5 * age + 5;
+        }else{
+            BMR = 10 * weight + 6.25 * height - 5 * age - 161;
+        }
+        // cuando se desborda pasa a ser infinito
+        float fBMR=(float)BMR;
+        if(Float.isInfinite(fBMR)){
+            throw new ArithmeticException("Se ha desbordado");
+        }
+        return fBMR;
+    
+	 
+    }
 }
